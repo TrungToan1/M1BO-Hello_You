32b810b6389320caa395629b7aacbff25fa4a74b
Ruviko Raid <shadowraid20@gmail.com>
2022-10-06 09:27:50 +0200

    updated backroomsexperience.py

--- backroomsexperience.py
+++ backroomsexperience.py
@@ -9,87 +9,91 @@ def typewriter(a): #first sentence
         time.sleep(0.03)
 os.system("cls")
 
-# a = "You're in bed. "
-# w = 0.5
-# typewriter(a) #Sentence 1
-# wait(w)
+a = "You're in bed. "
+w = 0.5
+typewriter(a) #Sentence 1
+wait(w)
 
-# a = "Trying to fall asleep after a long day of work.\n" 
-# w = 1
-# typewriter(a) #sentence 2
-# wait(w)
-# a = "You're bored of every day being the same."
-# typewriter(a)
-# wait(w)
-# a = "\nYou wish there was something else in life other than waking up in the same world.\n"
-# typewriter(a)
-# wait(w)
+a = "Trying to fall asleep after a long day of work.\n" 
+w = 1
+typewriter(a) #sentence 2
+wait(w)
+a = "You're bored of every day being the same."
+typewriter(a)
+wait(w)
+a = "\nYou wish there was something else in life other than waking up in the same world.\n"
+typewriter(a)
+wait(w)
 
-# a = ". "
-# w = 0.7
-# typewriter(a)
-# wait(w)
-# typewriter(a)
-# wait(w)
-# typewriter(a)
-# print("\n")
-# w = 1.5
-# wait(w)
+a = ". "
+w = 0.7
+typewriter(a)
+wait(w)
+typewriter(a)
+wait(w)
+typewriter(a)
+print("\n")
+w = 1.5
+wait(w)
 
-# a = "You can't fall asleep."
-# typewriter(a)
-# w = 1
-# wait(w)
+a = "You can't fall asleep."
+typewriter(a)
+w = 1
+wait(w)
 
-# w = 0.5
-# while True:
-#     print("\nA. Stay in bed.")
-#     wait(w)
-#     print("B. Stand up.")
-#     wait(w)
-#     q1 = input("\nType a or b: ")
-#     if q1 == "a":
-#         a = "\nYou stayed in bed. Hoping you'll wake up tommorow"
-#         typewriter(a) #a1
-#         break
-#     elif q1 == "b":
-#         a = "You stood up. You felt like taking a walk in a park nearby."
-#         w = 0.5
-#         typewriter(a) #b1 
-#         wait(w)
-#         a = "\nYou wore your jacket and shoes, ready to leave."
-#         wait(w)
-#         a = "\nOn your way to the nearby park, you see an abandoned factory.\n"
-#         typewriter(a)
-#         w = 1
-#         wait(w)
-
-#         w = 0.5
-#         while True:
-#             print("A. Continue.")
-#             wait(w)
-#             print("B. Go to the abandoned factory.")
-#             wait(w)
-#             q1b = input("\nType a or b: ")
-#             if q1b == "a":
-#                 a = "You didn't think much of it and headed towards the park"
-#                 typewriter(a)
-                
-#             elif q1b == "b":
-#                 a = "You were curious and decided to go to the abandoned factory."
-#                 typewriter(a)
-#                 w = 1
-#                 wait(w)
-#                 a = "\nYou entered the abandoned factory."
-#                 typewriter(a)
-#                 wait(w)
-#                 a = "\nYou suddenly feel static electricity in the air around you."
-#                 typewriter(a)
-#                 wait(w)
-#                 a = "\nAs you walk through the dark abandoned factory you feel the static electricity in the air getting stronger."
-#                 typewriter(a)
-#                 wait(w)
+w = 0.5
 while True:
+    print("\nA. Stay in bed.")
+    wait(w)
+    print("B. Stand up.")
+    wait(w)
+    q1 = input("\nType a or b: ")
+    if q1 == "a":
+        w = 1
+        a = "\nYou stayed in bed. Hoping you'll wake up tommorow"
+        typewriter(a) #a1
+        wait(w)
+        a = ""
+
+        break
+    elif q1 == "b":
+        a = "You stood up. You felt like taking a walk in a park nearby."
+        w = 0.5
+        typewriter(a) #b1 
+        wait(w)
+        a = "\nYou wore your jacket and shoes, ready to leave."
+        wait(w)
+        a = "\nOn your way to the nearby park, you see an abandoned factory.\n"
+        typewriter(a)
+        w = 1
+        wait(w)
+
+        w = 0.5
+        while True:
+            print("A. Continue.")
+            wait(w)
+            print("B. Go to the abandoned factory.")
+            wait(w)
+            q1b = input("\nType a or b: ")
+            if q1b == "a":
+                a = "You didn't think much of it and headed towards the park"
+                typewriter(a)
+                
+            elif q1b == "b":
+                a = "You were curious and decided to go to the abandoned factory."
+                typewriter(a)
+                w = 1
+                wait(w)
+                a = "\nYou entered the abandoned factory."
+                typewriter(a)
+                wait(w)
+                a = "\nYou suddenly feel static electricity in the air around you."
+                typewriter(a)
+                wait(w)
+                a = "\nAs you walk through the dark abandoned factory you feel the static electricity in the air getting stronger."
+                typewriter(a)
+                wait(w)
+                while True:
                     w = 0.5
                     print("\nA. Keep exploring")
                     wait(w)
@@ -104,7 +108,7 @@ while True:
                         a = "\nWhen you headed back to the exit you tripped over a piece of broken debris."
                         typewriter(a)
                         wait(w)
-                        a = "\nRight before you fell with your head on the ground,"
+                        a = "\nRight before you fell with your head on the ground, Everything turned dark."
                         typewriter(a)
 
                     elif q1bq == "b":
@@ -118,12 +122,12 @@ while True:
                         os.system("cls")
                         print("Error, try again")
                 
-    #         else:
-    #             os.system("cls")
-    #             print("Error, Try again.")
+            else:
+                os.system("cls")
+                print("Error, Try again.")
                
         
-    # else:
-    #     os.system("cls")
-    #     print("Error, Try again")
+    else:
+        os.system("cls")
+        print("Error, Try again")
         
