9b1c880959da650e2a424e58470804d6d4aab466
Ruviko Raid <shadowraid20@gmail.com>
2022-10-24 14:23:59 +0200

    start backroom code

---
+++ nextp.py
@@ -0,0 +1,92 @@
+import sys,time,os
+def wait(w):
+    time.sleep(w)
+
+def typewriter(a): #first sentence
+    for char in a:
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        time.sleep(0.01)
+os.system("cls")
+
+
+
+# w = 0.5
+# wait(w)
+# a = "\nYou are alone and disoriented, waist-deep in pool water. "
+# typewriter(a)
+# w = 1
+# wait(w)
+# a = "\nWading aimlessly around the interior room with wall-to-wall tile, your memory evades you."
+# typewriter(a)
+# wait(w)
+# a = "\nAfter making your way through a doorway bathed in light, the next room is a pool and the following room."
+# typewriter(a)
+# wait(w)
+# a = "\nShit."
+# typewriter(a)
+# wait(w)
+# a = "\nYour anxiety is swelling, but you soon find a small ledge to get out of the water."
+# typewriter(a)
+# wait(w)
+# a = "\nThere’s no time to relax."
+# typewriter(a)
+# wait(w)
+# a = "\nA shiver goes down your spine as you hear a mysterious splash off in the distance."
+# typewriter(a)
+# wait(w)
+# a = "\nWhile trying to get a clear look, something disappears behind the corner."
+# typewriter(a)
+# wait(w)
+# a = "\nYou continue walking along the ledge."
+# typewriter(a)
+# wait(w)
+# a = "\nThe splashing sounds closer."
+# typewriter(a)
+# wait(w)
+# a = "\nA walk turns into a jog which turns into a sprint."
+# typewriter(a)
+# wait(w)
+# a = "\nYou slip, fall, and hit your head against the wet tile as everything goes black."
+# typewriter(a)
+# wait(w)
+# a = "\nWhen you awake to the chlorine stench, it all starts to rush back."
+# typewriter(a)
+# wait(w)
+# a = "\nYou are trying to escape the Backrooms."
+# typewriter(a)
+# wait(w)
+
+w = 0.3
+while True:
+    print("A. Try to noclip back to the real world")
+    wait(w)
+    print("B. Explore\n")
+    wait(w)
+    br1 = input("Type a or b: ")
+    if br1 == "a":
+        a = "\nYou were teleported to a different building."
+        typewriter(a)
+        wait(w)
+        a = "\nEverything seems normal."
+        typewriter(a)
+        wait(w)
+        a = "\nYou looked around and observed your surroundings."
+        typewriter(a)
+        wait(w)
+        a = "\nYou walk around for a while and noticed that there's a sign on the wall that says YOU CHEATED."
+        typewriter(a)
+        wait(w)
+        a = "\nSuddenly.."
+        typewriter(a)
+        wait(w)
+        a = "\nPortions of the ceilings start to decline."
+        typewriter(a)
+        wait(w)
+        a = "\nHorrifying entities start to spawn."
+        typewriter(a)
+        wait(w)
+        a = "\nYour sanity is diminishing."
+    if br1 == "b":
+        print("You observe your environment.")
+        
