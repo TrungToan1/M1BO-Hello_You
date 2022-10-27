0e8994f3bcb2677084aaf1747cc444e3ef966880
TrungToan1 <34772@ma-web.nl>
2022-10-27 11:18:26 +0200

    backup nextp

--- nextp.py
+++ nextp.py
@@ -11,11 +11,11 @@ os.system("cls")
 
 
 
-# w = 0.5
+# w = 0
 # wait(w)
 # a = "\nYou are alone and disoriented, waist-deep in pool water. "
 # typewriter(a)
-# w = 1
+# w = 0
 # wait(w)
 # a = "\nWading aimlessly around the interior room with wall-to-wall tile, your memory evades you."
 # typewriter(a)
@@ -59,7 +59,7 @@ os.system("cls")
 
 w = 0.3
 while True:
-    print("A. Try to noclip back to the real world")
+    print("\n\nA. Try to noclip back to the real world")
     wait(w)
     print("B. Explore\n")
     wait(w)
@@ -112,21 +112,108 @@ while True:
             else:
                 print("Error, Try again")
                 print("--------------------")
-        break
+            break
+        a = "You see the Smiler slowly approaching you."
+        typewriter(a)
+        wait(w)
+        a = "You don't have much time to think."
+        while True:
+            print("\nA. Use your phone as a flashlight to shine it at the Smiler.")
+            wait(w)
+            print("B. Make a run for it")
+            smiler1 = input("\nType a or b: ")
+            if smiler1 == "a":
+                a = "You took your phone out and used the flashlight to scare off the smiler."
+                a = "Fire starts engulfing the environment."
+                a = "The chance of survival is slim but you don't give up just yet."
+                a = "You proceed consciously walking alongside the wall avoiding the flames that spread rapidly."
+                a = "It's been 30 minutes and you're slowly losing hope."
+                a = "Until.."
+                a = "The power shut off."
+                a = "And a door suddenly opened."
+                a = "Without hesitation you ran through the doorway and the room behind you exploded."
+                a = "The door behind closed."
+                a = 'A big floating message in the air says "NOBODY LIKES CHEATERS"'
+                a = "You've just realized you're in a maze."
+                a = "The fire continues engulfing the environment."
+                a = "You're carefully walking through a section of the maze."
+                a = "You see a skin-walker looking at you unaggravated."
+                while True:
+                    print("A. Show skin-walker information.")
+                    wait(w)
+                    print("B. Skip")
+                    skin_info = input("Type a or b: ")
+                    if skin_info == "a":
+                        print("Entity 10, more commonly known as the Skin-Stealers, are large humanoid entities that can wear the skin of their victims as a disguise.")
+                        wait(w)
+                        print("They eat human flesh when in a hunger state.")
+                        wait(w)
+                        print("")
+                    
+
     elif br1 == "b":
-                a = "You're present in an expansive non-Euclidean space, resembling the back rooms of a retail outlet."
-                a = "All rooms in Level 0 share the same superficial features, such as worn mono-yellow wallpaper, old moist carpet, scattered electrical outlets, and inconsistently-placed fluorescent lighting. "
-                a = "The fluorescent lighting hums at a constant frequency."
-                a = "Everything seems odd."
-                a = "This buzzing is notably louder and more obtrusive than ordinary fluorescent lights."
-                a = "You suddenly hear someone calling for help."
-                a = 'A noise that sounds like "Help me".'
+                a = "\nYou're present in an expansive non-Euclidean space, resembling the back rooms of a retail outlet."
+                typewriter(a)
+                wait(w)
+                a = "\nAll rooms share the same superficial features, worn mono-yellow wallpaper, old moist carpet, scattered electrical outlets, and \ninconsistently-placed fluorescent lighting. "
+                typewriter(a)
+                wait(w)
+                a = "\nThe fluorescent lighting hums at a constant frequency."
+                typewriter(a)
+                wait(w)
+                a = "\nEverything seems odd."
+                typewriter(a)
+                wait(w)
+                a = "\nThis buzzing is notably louder and more obtrusive than ordinary fluorescent lights."
+                typewriter(a)
+                wait(w)
+                a = "\nYou suddenly hear someone calling for help."
+                typewriter(a)
+                wait(w)
+                a = '\nA noise that sounds like "Help me".'
+                typewriter(a)
+                wait(w)
                 w = 0.3
                 while True:
-                    print("Locate where the noise comes from.")
+                    print("\n\nA. Locate where the sound comes from.")
                     wait(w)
-                    print("Move away from it and continue.")
-                    
+                    print("B. Move away from it and continue.")
+                    lvl_0 = input("\nType a or b: ")
+                    if lvl_0 == "a":
+                        a = "\nYou peeked around the corner of the wall next to you."
+                        typewriter(a)
+                        wait(w)
+                        a = "\nYou see a tall endoskeleton standing in the middle of an open room."
+                        typewriter(a)
+                        wait(w)
+                        a = "\nIn fear, you quickly but silently went the other direction until you heard a loud scream towards your direction."
+                        typewriter(a)
+                        wait(w)
+                        a = "\nAdrenaline rushed your body as your flight or fight response took action."
+                        typewriter(a)
+                        wait(w)
+                        a = "\nYou tried to run in a zig-zag motion to lose the potentially dangerous entity."
+                        typewriter(a)
+                        wait(w)
+                        a = "\nYou tripped because you have skill issue."
+                        typewriter(a)
+                        wait(w)
+                        a = "\nYou are paralyzed because of fear."
+                        typewriter(a)
+                        wait(w)
+                        a = "\nYou can hear the sound of heavy footsteps getting closer."
+                        typewriter(a)
+                        wait(w)
+                        a = "\nYou died."
+                        typewriter(a)
+                        wait(w)
+                        print("\nUnlocked ending: Deception") 
+                        break
+                    break            
+                break
+    else:
+        print("Error, Try again")
+        print("--------------------------------------------------")                 
 
 
         
